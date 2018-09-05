344563f1d0e442d0e4071865df2fa47a7b922b7e
Evgeny Kozhevnikov <evgeny.kozhevnikov@ooma.com>
2018-09-05 16:26:35 +0300

    LTE-Manager: FW upgrade

---
+++ SandBox/TeloModem/lte-manager/fw-upgrade.h
@@ -0,0 +1,54 @@
+/*
+ *  fw-upgrade.h
+ *  
+ *  Copyright (C) 2015-2018 Ooma Incorporated. All rights reserved.
+ *
+ */
+
+#pragma once
+
+#include <sys/inotify.h>
+#include <string>
+
+// forward declarations
+class ModemGTC;
+class Configuration;
+
+typedef enum
+{
+    FWU_IDLE,
+    FWU_READY_FOR_UPGRADE,
+    FWU__UPGRADE_COMPLETED,
+    FWU_UNKNOWN
+} FWU_STATE;
+
+#define EVENT_SIZE  ( sizeof (struct inotify_event) )
+#define BUF_LEN     ( 1024 * ( EVENT_SIZE + 16 ) )
+
+// singleton
+class FirmwareUpgrader
+{
+  public:
+    static FirmwareUpgrader &instance();
+
+    void configure(const Configuration &config);
+    bool checkForUpdate(std::string &fwFileName);
+    FWU_STATE getState() const { return state_; }
+    bool upgrade(ModemGTC &modem, const std::string &fwFileName);
+
+  private:
+    FirmwareUpgrader();
+    ~FirmwareUpgrader();
+
+    // disable copy constructor...
+    FirmwareUpgrader(FirmwareUpgrader const &) = delete;
+    // ... and operator =
+    FirmwareUpgrader &operator= (FirmwareUpgrader const &) = delete;
+
+    FWU_STATE state_;
+    int eventDescs_;
+    int watchDesc_;
+    char events_[BUF_LEN];
+};
+
+
